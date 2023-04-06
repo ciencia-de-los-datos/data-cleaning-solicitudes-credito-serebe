c0a463aa188497e0c6d390c68ad934459b3f8540
Sebastian Restrepo <64997621+serebe@users.noreply.github.com>
2023-04-05 23:59:17 -0500

    solucion preguntas1-9

--- pregunta.py
+++ pregunta.py
@@ -10,11 +10,33 @@ import pandas as pd
 
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    datos=pd.read_csv('solicitudes_credito.csv',sep=';',index_col='Unnamed: 0')
+    def may_min(datos):
+        columnas=datos.select_dtypes(object).columns
+        for i in columnas:
+            datos[i]=datos[i].str.lower()
+            datos[i]=datos[i].str.replace('_',' ',regex=True)
+            datos[i]=datos[i].str.replace('-',' ',regex=True)
+        datos=datos.dropna()  
+        return datos
+    datos=may_min(datos)
+    datos['monto_del_credito']=datos['monto_del_credito'].str.replace('$','')
+    datos['monto_del_credito']=datos['monto_del_credito'].str.replace('.','d')
+    datos['monto_del_credito']=datos['monto_del_credito'].str.replace(' ','')
+    datos['monto_del_credito']=datos['monto_del_credito'].str.replace('d00','')
+    datos['monto_del_credito']=datos['monto_del_credito'].str.replace(',','',regex=True)
+    datos['monto_del_credito']=datos['monto_del_credito'].astype(int)
+    datos['comuna_ciudadano']=datos['comuna_ciudadano'].astype(int)
+    datos['comuna_ciudadano']=datos['comuna_ciudadano'].astype(int)
+    datos['línea_credito']=datos['línea_credito'].str.replace(' ','',regex=True)
+    formatos_fecha = ['%Y/%m/%d', '%d/%m/%Y', '%Y-%m-%d', '%d-%m-%Y', '%m/%d/%Y', '%Y/%d/%m']
+    def convertir_fecha(cadena):
+        for formato in formatos_fecha:
+            try:
+                return pd.to_datetime(cadena, format=formato)
+            except:
+                pass
+        return np.nan
+    datos['fecha_de_beneficio'] = datos['fecha_de_beneficio'].apply(convertir_fecha)
+    df=datos.drop_duplicates()
     return df
